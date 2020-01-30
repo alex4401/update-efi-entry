0653115729f34f44db9aa012d8c6f95999c82fc9
alex4401 <rylatgl@gmail.com>
2020-01-30 14:01:45 +0100

    Allow non-PARTUUID system root

--- hooks/10-system-root-detection.sh
+++ hooks/10-system-root-detection.sh
@@ -2,16 +2,25 @@
 # Automatically find the system (/) partition's UUID.
 ######
 
-if [ -z "$ROOT_PARTITION_WRITABLE" ]
+if [ -z "$WRITABLE_ROOT" ]
 then
-    echo "Assuming that the root partition is desired to be writable."
-    ROOT_PARTITION_WRITABLE="yes"
+    echo "Assuming that the root partition is meant to be writable."
+    WRITABLE_ROOT="yes"
 fi
 
 # Exit early if the UUID is already specified.
-if [ -n "$ROOT_PARTITION_UUID" ]
+if [ -n "$SYSTEM_ROOT" ] && [ "$SYSTEM_ROOT" != "auto" ]
 then
     return 0
 fi
 
-ROOT_PARTITION_UUID=`findmnt -fn -o PARTUUID /`
+# Find the GPT UUID of current mount.
+_ROOT_PARTUUID=`findmnt -fn -o PARTUUID /`
+if [ -z "$_ROOT_PARTUUID" ]
+then
+    error "PARTUUID of system root could not be determined."
+    error "TODO: Auto-detection for LVM setups is not supported yet."
+    return 1
+fi
+
+SYSTEM_ROOT="PARTUUID=$_ROOT_PARTUUID"
